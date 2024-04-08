3aec9cf738058a4c0c4206eddfbe16a71898d481
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-04-08 16:15:04 +0330

    Add Middleware

--- src/App/Store.jsx
+++ src/App/Store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../Feautures/Counter/counterSlice";
 import numberReducer from "../Feautures/Counter/Number/numberSlice";
+import logger from "redux-logger"; //Redux-logger for middleware
 
 const store = configureStore({
     //Az yek Reducer baraye 2 action estefade shod
     reducer: { counter: counterReducer, number: numberReducer },
+    middleware: GetDefaultMiddleware => GetDefaultMiddleware().concat(logger), //Redux-logger for middleware
 })
 export default store;
