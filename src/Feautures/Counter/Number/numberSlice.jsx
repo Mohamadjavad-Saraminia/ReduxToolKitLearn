9a60e47aa5fead306e650741c49dd5468302997a
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-04-08 16:45:38 +0330

    Extra Reducer | Reducer Counter mitoone rooye number Eemal beshse

--- src/Feautures/Counter/Number/numberSlice.jsx
+++ src/Feautures/Counter/Number/numberSlice.jsx
@@ -1,5 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+//ba Esme incriment counter importesh kon
+import { increment as incrementCounter } from "../counterSlice";
+
 const initialState = {
     numberValue: 0,
 };
@@ -22,6 +26,12 @@ const numberSlice = createSlice({
             state.numberValue--;
         },
     },
+
+    //Har Vaght in Action dar counterSlice Ettefagh oftad; rooye numberSlice ham Eemal mishe
+    extraReducers: (builder) => {
+        builder.addCase(incrementCounter, (state) => { state.numberValue++; })
+    }
+
 });
 
 export default numberSlice.reducer;
